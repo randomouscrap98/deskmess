cb89af7da18f17e6e852c24b7c006f758b02f53e
haloopdy <randomouscrap98@aol.com>
2025-05-19 21:05:06 -0400

    Test harness no alert

--- test/test.js
+++ test/test.js
@@ -4,6 +4,13 @@
 // will need a page reload before they can be run. Tests are dependent on each other
 // unfortunately, as state needs to be tested.
 
+// Immediately disable alerts
+let TEST_alerts = [];
+window.altAlert = function(msg) {
+  console.warn(msg);
+  TEST_alerts.push(msg);
+}
+
 const RESUMETESTPARAM = "resumetest";
 var TEST_Current = 0;
 function TEST_ReloadPage(extraQuery) {
@@ -14,7 +21,7 @@ function TEST_ReloadPage(extraQuery) {
 
 var TESTS = [
   function() {
-    console.log("Trivial clicking on things not fail test");
+    console.log("Basic 'clicking on things' test");
     window['action-newtext'].click();
     window['action-newchat'].click();
     window['action-newtodo'].click();
@@ -45,13 +52,43 @@ var TESTS = [
     if(isHidden(window['tool-tray'])) {
       throw "Tooltray hidden after click";
     }
+    let cbd = function(id, active) {
+      if(window[id].classList.contains("deactivated") == active) {
+        throw "Expected active=" + active + ", didn't get that"
+      }
+    };
+    // IDC what these do, just make sure they don't crash on click or unclick
+    window['action-resetview'].click();
+    window['action-gridsnap'].click();
+    window['action-pageview'].click();
+    window['action-darkmode'].click();
+    window['action-modern'].click();
+    cbd('action-gridsnap', true);
+    cbd('action-pageview', true);
+    cbd('action-darkmode', true);
+    cbd('action-modern', true);
+    window['action-resetview'].click();
+    window['action-gridsnap'].click();
+    window['action-pageview'].click();
+    window['action-darkmode'].click();
+    window['action-modern'].click();
+    cbd('action-gridsnap', false);
+    cbd('action-pageview', false);
+    cbd('action-darkmode', false);
+    cbd('action-modern', false);
+  },
+  function() {
+    TEST_ReloadPage("server=test1");
+  },
+  function() {
+    console.log("Creating new test1 page");
   },
 ];
 
 window.addEventListener("load", function() {
   window.onerror = function customErrorHandler(msg, url, lineNo, columnNo, error) {
     // Log the error to the console or send it to a server
-    let output = `Error: ${msg}\nURL: ${url}\nLine Number: ${lineNo}\nColumn Number: ${columnNo}\nError Object: ${error}`;
+    let output = `FAIL ON TEST ${TEST_Current}\n\nError: ${msg}\nURL: ${url}\nLine Number: ${lineNo}\nColumn Number: ${columnNo}\nError Object: ${error}`;
     console.error(output);
     alert(output);
     // Return true to prevent the default error handling
@@ -71,6 +108,7 @@ window.addEventListener("load", function() {
     TEST_Current = Number(queryParams.get(RESUMETESTPARAM));
   }
   for(; TEST_Current < TESTS.length; TEST_Current++) {
+    console.log("RUNNING TEST " + TEST_Current);
     TESTS[TEST_Current]();
   }
   alert("All tests pass!");
